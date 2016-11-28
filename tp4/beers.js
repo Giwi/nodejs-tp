6084ad8474af56f514dc267b8bef922233b48055
Xavier MARIN <marin.xavier@gmail.com>
2016-11-28 17:05:08 +0100

    wip

--- tp4/beers.js
+++ tp4/beers.js
@@ -16,18 +16,13 @@ function Beer(id, alcohol, name, description) {
     this.alcohol = alcohol;
     this.name = name;
     this.description = description;
-}
 
-/**
- *
- * @param color
- * @returns {string}
- */
-Beer.prototype.toString = function (color) {
-    if (color) {
-        return this.name.toUpperCase().blue + '\t' + this.alcohol.toString().red + '\t' + this.description;
-    } else {
-        return this.name.toUpperCase() + '\t' + this.alcohol + '\t' + this.description;
+    this.toString = function (color) {
+        if (color) {
+            return this.name.toUpperCase().blue + '\t' + this.alcohol.toString().red + '\t' + this.description;
+        } else {
+            return this.name.toUpperCase() + '\t' + this.alcohol + '\t' + this.description;
+        }
     }
 };
 
